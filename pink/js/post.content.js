a1a31390bf6f26aeab059d38fe8e01d0c7126ab9
sheeye <sheeye@qq.com>
2023-06-06 17:33:25 +0800

    Update post.content.js

--- pink/js/post.content.js
+++ pink/js/post.content.js
@@ -13,7 +13,7 @@ const moreContent = (action, btn) => {
 };
 
 const imageLoad = (url, callback) => {
-  let img = new Image;
+  let img = new Image();
   img.src = url;
   if (img.complete) {
     callback(img.width, img.height);
@@ -26,22 +26,76 @@ const imageLoad = (url, callback) => {
 };
 
 const viewImage = (url, thumbnail) => {
-  let textPrefix = '';
+  let urls = [];
+  let index = 0;
+  let lock = false;
   if (thumbnail.tagName.toLowerCase() == 'img') {
-    let pagedInfo = childIndex(thumbnail.parentNode);
-    textPrefix = pagedInfo[1] == 1 ? '' : pagedInfo[0] + '/' + pagedInfo[1] + ' ';
+    /*
+    gallery
+    ..figure
+      ..thumbnail
+    */
+    for (let child of thumbnail.parentNode.parentNode.children) urls.push(child.firstElementChild.getAttribute('data-index'));
+    index = [].indexOf.call(urls, url);
+  } else {
+    urls.push(url);
   }
-  let gallery = document.createElement('figure');
-  gallery.className = 'gallery-view';
-  gallery.innerHTML = '<span class="view-loading">' + textPrefix + 'loading...</span>';
-  document.body.append(gallery);
-  setTimeout(() => {
-    imageLoad(url, () => {
-      gallery.innerHTML = '<img class="view-image" src="' + url + '" alt="" />';
-    });
-    gallery.addEventListener('click', e => {
-      gallery.parentNode.removeChild(gallery);
-      e.stopPropagation();
-    }, true);
-  }, 250);
+  let viewer = new DOMParser().parseFromString(`<div class="viewer viewer-keyaction-close" data-action="close">
+                                                  <figure class="viewer-image"></figure>
+                                                  <div class="viewer-controls">
+                                                    <div class="viewer-count g-inline-center g-txt-ellipsis"><span class="viewer-current">${index}</span>/${urls.length}</div>
+                                                    <nav class="viewer-navigation">
+                                                      <div class="viewer-btn g-inline-center g-txt-ellipsis viewer-keyaction-prev" data-action="prev">prev</div>
+                                                      <div class="viewer-btn g-inline-center g-txt-ellipsis viewer-keyaction-next" data-action="next">next</div>
+                                                    </nav>
+                                                  </div>
+                                                </div>`, 'text/html').body.firstElementChild;
+  document.body.append(viewer);
+  let imageSwitch = (index, init = false) => {
+    lock = true;
+    if (urls.length > 1 && init) viewer.querySelector('.viewer-controls').classList.add('show');
+    viewer.querySelector('.viewer-current').innerHTML = index + 1;
+    viewer.querySelector('.viewer-image').innerHTML = '<div class="viewer-loading g-txt-hide">loading...</div>';
+    setTimeout(() => {
+      imageLoad(urls[index], () => {
+        viewer.querySelector('.viewer-image').innerHTML = '<img src="' + urls[index] + '" draggable="false" alt="" />';
+        lock = false;
+      });
+    }, init ? 500 : 250);
+  };
+  let keyAction = e => {
+    let keyMap = {Escape: 'close', ArrowLeft: 'prev', ArrowRight: 'next'};
+    if (keyMap[e.key]) {
+      /* viewer.querySelect... will report an error */
+      document.body.querySelector(`.viewer-keyaction-${keyMap[e.key]}`).click();
+      e.preventDefault();
+    }
+  };
+  imageSwitch(index, true);
+  window.addEventListener('keydown', keyAction);
+  viewer.onclick = e => {
+    if (e.target.hasAttribute('data-action')) {
+      let action = e.target.getAttribute('data-action');
+      switch (action) {
+        case 'close':
+          window.removeEventListener('keydown', keyAction);
+          viewer.onclick = null;
+          viewer.parentNode.removeChild(viewer);
+          break;
+        case 'prev':
+          if (!lock) {
+            index = index == 0 ? urls.length - 1 : index - 1;
+            imageSwitch(index);
+          }
+          break;
+        case 'next':
+          if (!lock) {
+            index = index == urls.length - 1 ? 0 : index + 1;
+            imageSwitch(index);
+          }
+          break;
+      }
+    }
+    e.stopPropagation();
+  };
 };
